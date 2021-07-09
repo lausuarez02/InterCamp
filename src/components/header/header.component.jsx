4958237c2436e9ab1960e38ea8e63136f19569ed
lausuarez02 <lausuarez71@gmail.com>
2021-07-09 23:02:39 +0200

    Intercamp logo when user login redirect to quiz https://github.com/lausuarez02

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -10,28 +10,45 @@ import StarFavorite from "../star-favorite/star-favorite.component";
 
 const Header = ({ currentUser, favQuestions }) => {
   return (
-    <div className="header">
-      <Link className="logo-container" to="/">
-        <div className="logo-image-container">
-          <img className="logo-image" src={Logo} alt="website logo" />
-        </div>
-        <h3 className="website-name">{header.websiteName}</h3>
-      </Link>
-      <div className="button-container">
+  
+   
+    <div>
+   
         {currentUser && favQuestions ? (
+           <div className="header">
+           <Link className="logo-container" to="/quiz">
+             <div className="logo-image-container">
+               <img className="logo-image" src={Logo} alt="website logo" />
+             </div>
+             <h3 className="website-name">{header.websiteName}</h3>
+           </Link>
+
+           <div className="button-container">
           <div className="user-fav-container">
             <StarFavorite />
             <Link to="/user">
               <AccountIcon />
             </Link>
           </div>
+          </div>
+          </div>
         ) : (
+          <div className="header">
+          <Link className="logo-container" to="/">
+            <div className="logo-image-container">
+              <img className="logo-image" src={Logo} alt="website logo" />
+            </div>
+            <h3 className="website-name">{header.websiteName}</h3>
+          </Link>
+          <div className="button-container">
           <Link to={header.signInLink}>
             <CustomButtonOutlined>{header.signInLabel}</CustomButtonOutlined>
           </Link>
+        </div>
+        </div>  
         )}
       </div>
-    </div>
+
   );
 };
 
